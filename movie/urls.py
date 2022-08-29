8213f2d4b64333dc666f09fdd92c618b6d405f71
Carrie8348 <maruko_carrie@hotmail.com>
2022-08-29 15:17:46 +0000

    added movie template and home template

--- movie/urls.py
+++ movie/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.MovieList.as_view, name='home'),
+    path('', views.MovieList, name='movies'),
     #path('movie_detail/', views.MovieDetail.as_view(), name='movie_detail'),
     #path('like/', views.MovieLikes.as_view(), name='movie_like'),
     #path('dislike/', views.MovieDislikes.as_view(), name='movie_dislike'),
--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
-from django.views import generic
 from .models import Movie
 
 # Create your views here.
-class MovieList(generic.ListView):
-    model = Movie
-    queryset = Movie.objects.filter(status=1).order_by('-updated_on')
-    template_name = 'home.html'
-    paginate_by = 4
+def MovieList(request):
+    """ A view to show all products, including sorting and search queries """
+
+    movies = Movie.objects.all()
+    query = None
+    sort = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                movies = movies.annotate(lower_name=Lower('name'))
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('movies'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            movies = movies.filter(queries)
+
+    current_sorting = f'{sort}'
+
+    context = {
+        'movies': movies,
+        'search_term': query,
+        'current_sorting': current_sorting,
+    }
+
+    return render(request, 'movie/movies.html', context)
